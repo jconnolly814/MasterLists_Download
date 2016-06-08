6a528881d035b4f90c88bf14ef2b3ce8ce77f835
Jennifer Connolly <je.connolly@gmail.com>
2016-06-08 19:24:46 -0400

    create loop- export changes

--- FWS_CheckForChanges.py
+++ FWS_CheckForChanges.py
@@ -1,70 +1,41 @@
 __author__ = 'JConno02'
 
-from bs4 import BeautifulSoup
-import requests
-import sys
-import os
-
-
-import pandas as pd
-
-import csv
-
-oldMaster =''
-newMaster
-
-
-__author__ = 'JConno02'
-
-from bs4 import BeautifulSoup
-import requests
-import sys
+import datetime
 import os
 
 import pandas as pd
 
 import csv
 
-##Todo rather than loading in dict add a field to df that is in out and pop field based on if ent in on filter list then select all rows that are in
-date = 2016060722
-inpath_FULLTESS =r'C:\Users\JConno02\Documents\Projects\ESA\MasterLists\TESSQueries\20160606\FilteredinPandas\FullTess_20160607.csv'
-outpath = r'C:\Users\JConno02\Documents\Projects\ESA\MasterLists\TESSQueries\20160606\FilteredinPandas'
-url = "http://ecos.fws.gov/services/TessQuery?request=query&xquery=/SPECIES_DETAIL"
+date = 20160608
+oldMaster = r'C:\Users\JConno02\Documents\Projects\ESA\MasterLists\April2015\CSV\MasterListESA_April2015_20151015_20151124.csv'
+newMaster = r'C:\Users\JConno02\Documents\Projects\ESA\MasterLists\TESSQueries\20160606\FilteredinPandas\FilteredTessPandas_20160607.csv'
+table_id_list=['old', 'new']
+outpath = r'C:\Users\JConno02\Documents\Projects\ESA\MasterLists\TESSQueries\20160606\FilteredinPandas\Changes'
 
-r = requests.get(url)
-reload(sys)
-sys.setdefaultencoding('utf8')
-## These are the variables use within the xml, if FWS changes their variable this will need to be updated
+CheckForChanges_col = ['entity_id', 'spcode', 'vipcode', 'sciname', 'comname', 'family', 'status_text', 'country']
 
-identifier='entity_id'
-deafultTags = ['spcode', 'vipcode', 'sciname', 'comname', 'invname', 'pop_abbrev', 'pop_desc', 'family', 'status',
-               'status_text', 'lead_agency', 'lead_region', 'country', 'listing_date', 'dps', 'refuge_occurrence',
-               'delisting_date']
+newColindex = {'entity_id': 1,
+               'spcode': 10,
+               'vipcode': 11,
+               'sciname': 3,
+               'comname': 2,
+               'family': 9,
+               'status_text': 6,
+               'country': 14}
+oldColindex = {'entity_id': 0,
+               'spcode': 26,
+               'vipcode': 27,
+               'sciname': 8,
+               'comname': 7,
+               'family': 9,
+               'status_text': 11,
+               'country': 13}
 
-
-colOrder = {
-    9: 'spcode',
-    10: 'vipcode',
-    2: 'sciname',
-    1: 'comname',
-    3: 'invname',
-    6: 'pop_abbrev',
-    7: 'pop_desc',
-    8: 'family',
-    4: 'status',
-    5: 'status_text',
-    11: 'lead_agency',
-    12: 'lead_region',
-    13: 'country',
-    14: 'listing_date',
-    15: 'dps',
-    16: 'refuge_occurrence'}
-
-
-def CreatDicts(list_vars,id):
+def CreatDicts(list_vars, table_id):
     for v in list_vars:
-        globals()[v] = {}
-    print '\nSpecies information that will be extracted {0} using the identifier {1}'.format(list_vars, id)
+        dictname = "{0}_{1}".format(v, table_id)
+        globals()[dictname] = {}
 
 def list_dicts():
     # global spcode, vipcode, sciname, comname, invname, pop_abbrev, pop_desc, family, status, status_text, lead_agency, lead_region, country, listing_date, dps, refuge_occurrence
@@ -76,114 +47,84 @@ def list_dicts():
     for value in listitems:
         if type((globals()[value])) is dict:
             listdicts.append(value)
-    listdicts.remove('colOrder')
     return listdicts
 
-
-def LoadDatainDicts (incsv,id):
-    allEntities =[]
-    dfFullTess = pd.read_csv(incsv)
-    header= list(dfFullTess.columns.values)
-    print header
-    entidindex = (header.index(id))
-    print entidindex
-    rowcount = dfFullTess.count(axis=0, level=None, numeric_only=False)
+def LoadDatainDicts(incsv, colIndex, dict_list,table_id):
+    ent_df = pd.DataFrame(columns=['EntityID'])
+    df= pd.read_csv(incsv)
+    header = list(df.columns.values)
+    #print header
+    entidindex = colIndex["entity_id"]
+    #print entidindex
+    rowcount = df.count(axis=0, level=None, numeric_only=False)
     rowindex = rowcount.values[0]
-    colindex = len(header)-1 # to make base 0
+    colindex = len(header) - 1  # to make base 0
 
     row = 0
     while row < (rowindex):
-        entid = str(dfFullTess.iloc[row, entidindex]) #Entid must  be in position 0
-        print "Working on species {0}, row {1}".format(entid, row)
+        entid = str(df.iloc[row, entidindex])
+        ent_df=ent_df.append({'EntityID': entid}, ignore_index=True)
+        #print "Working on species {0}, row {1}".format(entid, row)
+        for dictionary in dict_list:
+            if dictionary.endswith(table_id):
+                str_remove= "_" +str(table_id)
+                colkey = dictionary.replace(str_remove,'')
+                col = colIndex[colkey]  ## assuming an index row from pandas export
+                value = df.iloc[row, col]
+                ((globals()[dictionary][entid])) = str(value)
 
-        allEntities.append(str(entid))
-        col=1 ## assuming an index row from pandas export
-        while col <(colindex+1): # base 0
-            if col == entidindex:
-                col+=1
-                continue
-            else:
-                colheader_dict = header[col]
-                value = dfFullTess.iloc[row, col]
-                ((globals()[colheader_dict][entid]))= str(value)
-                col+=1
         row += 1
-    print allEntities
-    return allEntities
+    addSpecies_df = df[df['EntityID'].isin(ent_df['EntityID'])]
+    return ent_df
 
-
-def filterquery(listvars, entlist):
-    filterentlist = []
-    possAnswer = ['Yes', 'No']
-    askQ = True
-    while askQ:
-        status = raw_input('Where is the status located {0}: '.format(listvars))
-        if status not in listvars:
-            print 'This is not a valid answer: remove quotes and spaces'
+def CheckForChanges(dictlist,cols,allent_df):
+    for v in cols:
+        start =datetime.datetime.now()
+        if v== 'entity_id':
+            continue
         else:
-            liststatus = set(globals()[status].values())
-            break
-
-    while askQ:
-        country = raw_input('Where is the country  located {0}: '.format(listvars))
-        if country not in listvars:
-            print 'This is not a valid answer: remove quotes and spaces'
-        else:
-            break
-    Failed = False
-    while askQ:
-        StatusConsidered = ['Experimental Population  Non-Essential', 'Threatened', 'Endangered', 'Proposed Threatened',
-                            'Proposed Endangered', 'Candidate']
-
-        defaultStatus = raw_input(
-            'The current status being considered are:  \n{0}\nWould you like to add additional statuses? : Yes or No: '.format(
-                StatusConsidered))
-
-        if defaultStatus not in possAnswer:
-            print('This is not a valid answer, must be Yes or No')
-        else:
-            if defaultStatus == 'Yes':
-                filtered_status = raw_input('Which statuses should be included in the filtered table{0}? '.format(
-                    liststatus))  ##TODO print out list one value per line
-                listinput = filtered_status.split(",")
-                for v in listinput:
-                    if v not in liststatus:
-                        print v
-                        Failed = True
-                        print 'This is not a valid answer: values must be separated by a comma without a space'
-                    else:
-                        if v not in StatusConsidered:
-                            StatusConsidered.append(v)
-
+            print "Checking for changes in {0}".format(v)
+            current=[]
+            for dictionay in dictlist:
+                if dictionay .startswith(v):
+                    current.append(dictionay)
+            if len(current)!= 2:
+                print "Only one dictionary can't compare {0}".format(current)
             else:
-                if Failed == False:
-                    askQ = False
-                else:
-                    askQ = True
 
-    for i in entlist:
-        check_status = globals()[status][i]
+                rowcount = allent_df.count(axis=0, level=None, numeric_only=False)
+                rowindex = rowcount.values[0]
+                row =0
+                ent_df = pd.DataFrame(columns=['EntityID','Old Value','New Value','Change'])
+                while row < (rowindex):
+                    entid = str(allent_df.iloc[row,0])
+                    for value in current:
+                        if value.endswith('new'):
+                            try:
+                                new= ((globals()[value][entid]))
 
-        if check_status in StatusConsidered:
-            filterentlist.append(i)
+                            except KeyError:
+                                new= 'NA'
+                        else:
+                            try:
+                                old= ((globals()[value][entid]))
 
-    for i in filterentlist:
-        check_country = str(globals()[country][i])
-        check_country = " ".join(check_country.split())
-        print i
-        print check_country
-        if check_country == '2':
-            filterentlist.remove(i)
-            print 'removed {0}'.format(i)
-        elif check_country == 'None':
-            filterentlist.remove(i)
-            print 'removed None {0}'.format(i)
-        else:
-            pass
+                            except KeyError:
+                                old= 'NA'
+                    row +=1
+                    if old == new:
+                        change = 'No'
+                    else:
+                        change= 'Yes'
+                    #result =pd.DataFrame([entid,old,new,change])
+                    #ent_df =ent_df.append(result)
+                    ent_df =ent_df.append({'EntityID':entid,'Old Value':old,'New Value':new,'Change':change},ignore_index=True)
 
-    print filterentlist
-    return filterentlist
 
+                ent_df = ent_df[ent_df['Change'].isin(['Yes'])]
+                outcsv = outpath + os.sep + 'Changes_' + str(v) + '.csv'
+                ent_df.to_csv(outcsv)
+                print "Exported {0} completed in {1}".format(outcsv, (datetime.datetime.now()-start))
 
 def CreateSpecisTable(species_entList, species_info_var, colOrder):
     list_cols = colOrder.values()
@@ -222,7 +163,6 @@ def CreateSpecisTable(species_entList, species_info_var, colOrder):
         outlist.append(current_species)
     return outlist, header
 
-
 def create_outtable(outInfo, csvname, header):
     ## CHANGE added a function to export to the dictionaries and lists to csv to QA the intermediate steps and to have copies of the final tables
     if type(outInfo) is dict:
@@ -242,31 +182,47 @@ def create_outtable(outInfo, csvname, header):
                 writer.writerow([val])
 
 
-CreatDicts(deafultTags,identifier)
-sp_info_need = list_dicts()
-fulllist= LoadDatainDicts(inpath_FULLTESS,identifier)
+CreatDicts(CheckForChanges_col, "old")
+CreatDicts(CheckForChanges_col, "new")
+allDicts = list_dicts()
 
-filter_list = filterquery(sp_info_need,fulllist)
-print "There are {0} listed entities that will be considered".format(len(filter_list))
-
-filterResults, header = CreateSpecisTable(filter_list, deafultTags, colOrder)
-
-finalheader = ['EntityID']
-for v in header:
-    finalheader.append(v)
-
-for value in sp_info_need:
-    create_outtable(globals()[value], (outpath+ os.sep+ str(value)+"_"+str(date)+'.csv') , finalheader)
+ent_df_old =LoadDatainDicts(oldMaster,oldColindex,allDicts,"old")
+ent_df_new =LoadDatainDicts(newMaster,newColindex,allDicts,"new")
 
 
-outfilter = outpath + os.sep + 'FilteredTess_' + str(date) + '.csv'
+addSpecies_df = ent_df_new[ent_df_new['EntityID'].isin(ent_df_old['EntityID'])==False]
+removedspecies_df = ent_df_old[ent_df_old['EntityID'].isin(ent_df_new['EntityID'])==False]
+
+allents_singlelist= ent_df_old.append([ent_df_new])
+allents_singlelist=allents_singlelist.drop_duplicates()
 
 
 
-outDF_filtered = pd.DataFrame(filterResults, columns=finalheader)
-outDF_filtered.to_csv(outfilter, encoding='utf-8')
+add_outfilter = outpath + os.sep + 'added_TESS' + str(date) + '.csv'
+remove_outfilter =outpath + os.sep + 'RemovedTESS_' + str(date) + '.csv'
+
+addSpecies_df .to_csv(add_outfilter , encoding='utf-8')
+removedspecies_df.to_csv(remove_outfilter , encoding='utf-8')
+
+CheckForChanges(allDicts,CheckForChanges_col,allents_singlelist)
+
+#print "Added Species {0}".format(addSpecies_df)
+#print "Removed Species {0}".format(removedspecies_df)
+
+
+
+# finalheader = ['EntityID']
+# for v in header:
+#     finalheader.append(v)
+#
+# for value in sp_info_need:
+#     create_outtable(globals()[value], (outpath + os.sep + str(value) + "_" + str(date) + '.csv'), finalheader)
+#
+# outfilter = outpath + os.sep + 'FilteredTess_' + str(date) + '.csv'
+#
+# outDF_filtered = pd.DataFrame(filterResults, columns=finalheader)
+# outDF_filtered.to_csv(outfilter, encoding='utf-8')
 
 
 
 # create_outtable(filterResults, outfilter, finalheader)
-
